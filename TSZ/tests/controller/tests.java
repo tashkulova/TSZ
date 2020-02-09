0d3b1d2548240510d223f91c4675cffe18bf728f
tashkulova <diana.tashkulova@gmail.com>
2020-02-09 14:44:33 +0100

    start a projekt "TSZ"

---
+++ TSZ/tests/controller/tests.java
@@ -0,0 +1,13 @@
+package controller;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class tests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
